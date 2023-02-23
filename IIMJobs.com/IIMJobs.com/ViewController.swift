ece9687bd19b5f7aa0534cc08418430289b53f9a
mradulatray <mradulatreyhts@gmail.com>
2023-02-23 14:03:00 +0530

    Added Project

---
+++ IIMJobs.com/IIMJobs.com/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IIMJobs.com
+//
+//  Created by mradulatray on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
